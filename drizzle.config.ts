2e266e3aca0b7b9b7239d74dd006ecbc2a3bf1ed
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2024-07-01 01:11:40 -0400

    Remove password from repo and add documentation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgres://postgres:postgres@127.0.0.1:5432/postgres"
+    url: `postgres://postgres:${process.env.POSTGRES_PASSWORD}@127.0.0.1:5432/postgres`
   }
 });
