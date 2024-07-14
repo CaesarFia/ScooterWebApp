ca758ccce0093c3eb2573d228eba70d96554e4fd
fernandofiallos <FernandoFrace@icloud.com>
2024-07-14 19:11:50 -0400

    Frontend for login pop up, map, sidebar, profile circle.

---
+++ src/components/MapComponent.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+    import { onMount } from 'svelte';
+    import mapboxgl from 'mapbox-gl';
+  
+    let map: mapboxgl.Map;
+  const mapboxAccessToken = 'pk.eyJ1IjoiY2Flc2FyZmlhIiwiYSI6ImNseTdxanEwNzA5aXQycW9lMDh6YmFrc2UifQ.CpwPntFIV276Nb755rg-Pg';
+
+
+  onMount(() => {
+    mapboxgl.accessToken = mapboxAccessToken;
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/caesarfia/clxzna0zz006q01p80zkl9v03'
+    });
+
+  });
+  </script>
+  
+  <style>
+    .overlay {
+      position: absolute;
+      top: 20%;
+      left: 50%;
+      transform: translate(-50%, -20%);
+      z-index: 1;
+    }
+  </style>
+  
+  <div id="map" class="absolute h-screen z-0 w-screen m-0 p-0"></div>
+  <div class="overlay">
+    <slot></slot>
+  </div>
+  
