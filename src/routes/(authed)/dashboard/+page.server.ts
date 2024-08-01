668446677002290488732fcf54f4a4d946b873f3
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2024-08-01 07:41:33 -0400

    Fixup PR - fix format - fix type issues - add auth guards - fixed employee insertion - add scooter data validation

--- src/routes/(authed)/dashboard/+page.server.ts
+++ src/routes/(authed)/dashboard/+page.server.ts
@@ -1,90 +1,132 @@
-import { lucia } from "$lib/server/auth";
-import { fail, redirect, type Actions } from "@sveltejs/kit";
-import { generateIdFromEntropySize } from "lucia";
 import { hash } from "@node-rs/argon2";
+import { error, type Actions } from "@sveltejs/kit";
+import { generateIdFromEntropySize } from "lucia";
 
 import db from "$lib/db";
-import { check } from "drizzle-orm/pg-core";
-import { scooters, users} from "$lib/db/schema";
-
-function isValidEmail(email: string): boolean {
-	return /.+@.+/.test(email);
-}
+import { employees, scooters, users } from "$lib/db/schema";
+import { isValidEmail, isValidPassword } from "$lib/utils";
 
 export const actions: Actions = {
-	make_scooter: async (event) => {
-		const formData = await event.request.formData();
-		const latitude = formData.get("latitude");
-		const longitude = formData.get("longitude");
-        const checked_out = formData.has("checked_out");
-		const need_repairs = formData.has("need_repairs");
-		const battery = formData.get("battery");
-		const rentals = formData.get("rentals");
+    make_scooter: async (event) => {
+        const formData = await event.request.formData();
 
-		const scooterId = generateIdFromEntropySize(10); // 16 characters long
-		
-		await db.insert(scooters).values({
-			id: scooterId,
-		    latitude: latitude,
+        const latitude = Number(formData.get("latitude")?.toString());
+        const longitude = Number(formData.get("longitude")?.toString());
+        const checkedOut = formData.has("checked_out");
+        const needRepairs = formData.has("need_repairs");
+        const battery = Number(formData.get("battery")?.toString());
+
+        if (
+            typeof latitude !== "number" ||
+            isNaN(latitude) ||
+            latitude < -90 ||
+            latitude > 90
+        ) {
+            error(400, {
+                message: "Invalid latitude"
+            });
+        }
+
+        if (
+            typeof longitude !== "number" ||
+            isNaN(longitude) ||
+            longitude < -180 ||
+            longitude > 180
+        ) {
+            error(400, {
+                message: "Invalid longitude"
+            });
+        }
+
+        if (
+            typeof battery !== "number" ||
+            isNaN(battery) ||
+            battery < 0 ||
+            battery > 100
+        ) {
+            error(400, {
+                message: "Invalid battery"
+            });
+        }
+
+        const scooterId = generateIdFromEntropySize(10); // 16 characters long
+        
+        await db.insert(scooters).values({
+            id: scooterId,
+            latitude: latitude,
             longitude: longitude,
-            checkedOut: checked_out,
-			needRepairs: need_repairs,
-			battery: battery,
-            rentals: rentals
-		});
+            checkedOut: checkedOut,
+            needRepairs: needRepairs,
+            battery: battery,
+        });
     },
 
 
-    make_user: async (event) => {
-		const formData = await event.request.formData();
-		const firstname = formData.get("firstname");
-		const lastname = formData.get("lastname");
-		const email = formData.get("email");
-		const password = formData.get("password");
-        const is_admin = formData.get("is_admin")
-		// username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
-		// keep in mind some database (e.g. mysql) are case insensitive
-		if (
-			typeof email !== "string" ||
-			!isValidEmail(email)
-		) {
-			return fail(400, {
-				message: "Invalid email"
-			});
-		}
-		if (typeof password !== "string" || password.length < 6 || password.length > 255) {
-			return fail(400, {
-				message: "Invalid password"
-			});
-		}
-		if (typeof firstname !== "string" || typeof lastname !== "string")
-			return fail(400, {
-				message: "Invalid name"
-			});
+    make_user: async ({request, locals}) => {
+        if (!locals.user || !locals.user.isAdmin) {
+            error(403, { message: "Forbidden" });
+        }
+        
+        const formData = await request.formData();
+        const firstname = formData.get("firstname");
+        const lastname = formData.get("lastname");
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const isEmployee = formData.has("is_employee");
+        const isAdmin = formData.has("is_admin")
+        // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
+        // keep in mind some database (e.g. mysql) are case insensitive
+        if (
+            typeof email !== "string" ||
+            !isValidEmail(email)
+        ) {
+            error(400, {
+                message: "Invalid email"
+            });
+        }
+        if (typeof password !== "string" || !isValidPassword(password)) {
+            error(400, {
+                message: "Invalid password"
+            });
+        }
+        if (typeof firstname !== "string" || typeof lastname !== "string")
+            error(400, {
+                message: "Invalid name"
+            });
 
-		const userId = generateIdFromEntropySize(10); // 16 characters long
-		const passwordHash = await hash(password, {
-			// recommended minimum parameters
-			memoryCost: 19456,
-			timeCost: 2,
-			outputLen: 32,
-			parallelism: 1
-		});
-		
-		await db.insert(users).values({
-			id: userId,
-			firstname: firstname,
+        const userId = generateIdFromEntropySize(10); // 16 characters long
+        const passwordHash = await hash(password, {
+            // recommended minimum parameters
+            memoryCost: 19456,
+            timeCost: 2,
+            outputLen: 32,
+            parallelism: 1
+        });
+        
+        await db.insert(users).values({
+            id: userId,
+            firstname: firstname,
             lastname: lastname,
             email: email,
-			passwordHash: passwordHash,
-			isAdmin: is_admin
-		});
-	}
+            passwordHash: passwordHash,
+        });
+
+        if (isEmployee) {
+            await db.insert(employees).values({
+                id: userId,
+                isAdmin: isAdmin,
+            });
+        }
+    }
 
 };
 
 /** @type {import('./$types').PageLoad} */
-export async function load() {
+export async function load({ locals }) {
+    if (!locals.user || !locals.user.isAdmin) {
+        error(403, { message: "Forbidden" });
+    }
+
     const scooterList = await db.select().from(scooters);
     const userList = await db.select().from(users);
     console.log(scooterList)
--- src/routes/(authed)/dashboard/+page.svelte
+++ src/routes/(authed)/dashboard/+page.svelte
@@ -1,16 +1,18 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+
+	let isEmployee = false;
 </script>
 
 <h1>Sign up</h1>
 <div class="make scooter">
 	<form action="?/make_scooter" method="POST">
 		<label for="latitude">Latitude</label>
-		<input type="text" name="latitude" id="latitude" required /><br />
+		<input type="number" name="latitude" id="latitude" required /><br />
 
 		<label for="longitude">Longitude</label>
-		<input type="text" name="longitude" id="longitude" required /><br />
+		<input type="number" name="longitude" id="longitude" required /><br />
 
 		<label for="checked_out">Checked Out</label>
 		<input type="checkbox" name="checked_out" id="checked_out" /><br />
@@ -20,9 +22,6 @@
 
 		<label for="battery">Battery (%)</label>
 		<input type="number" name="battery" id="battery" min="0" max="100" required /><br />
-
-		<label for="rentals">Rentals</label>
-		<input type="number" name="rentals" id="rentals" min="0" required /><br />
 
 		<button>Submit</button>
 	</form>
@@ -39,11 +38,16 @@
 		<label for="email">Email</label>
 		<input type="text" name="email" id="email" required /><br />
 
-		<label for="is_admin">Is Admin</label>
-		<input type="checkbox" name="is_admin" id="is_admin" /><br />
+		<label for="is_employee">Is Employee?</label>
+		<input type="checkbox" name="is_employee" id="is_employee" bind:value={isEmployee}/><br />
+
+		{#if isEmployee}
+			<label for="is_admin">Is Admin?</label>
+			<input type="checkbox" name="is_admin" id="is_admin" /><br />
+		{/if}
 
 		<label for="password">Password</label>
-		<input type="password" name="password" id="password" /><br />
+		<input type="password" name="password" id="password" required/><br />
 
 		<button>Submit</button>
 	</form>
@@ -65,8 +69,8 @@
 				<tr>
 					<td>{scooter.latitude}</td>
 					<td>{scooter.longitude}</td>
-					<td>{scooter.checked_out ? 'Yes' : 'No'}</td>
-					<td>{scooter.need_repairs ? 'Yes' : 'No'}</td>
+					<td>{scooter.checkedOut ? 'Yes' : 'No'}</td>
+					<td>{scooter.needRepairs ? 'Yes' : 'No'}</td>
 					<td>{scooter.battery}</td>
 				</tr>
 			{/each}
