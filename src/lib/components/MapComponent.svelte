4278d257be0c0665ca33c54ca4968b000533f335
Andrey Nikitin <143126464+pyukey@users.noreply.github.com>
2024-08-04 18:04:04 -0400

    Initial marker

--- src/lib/components/MapComponent.svelte
+++ src/lib/components/MapComponent.svelte
@@ -1,18 +1,20 @@
 <script lang='ts'>
     import { onMount } from 'svelte';
     import mapboxgl from 'mapbox-gl';
+    import 'mapbox-gl/dist/mapbox-gl.css'
   
     let map: mapboxgl.Map;
   const mapboxAccessToken = 'pk.eyJ1IjoiY2Flc2FyZmlhIiwiYSI6ImNseTdxanEwNzA5aXQycW9lMDh6YmFrc2UifQ.CpwPntFIV276Nb755rg-Pg';
 
 
-  onMount(() => {
+  onMount( () => {
     mapboxgl.accessToken = mapboxAccessToken;
     map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/caesarfia/clxzna0zz006q01p80zkl9v03'
     });
-  });
+    new mapboxgl.Marker({scale: 1, anchor: 'center'}).setLngLat([-82.347999, 29.644323]).addTo(map);
+    });
   </script>
     
   <div id="map" class="absolute h-screen z-0 w-screen m-0 p-0">
