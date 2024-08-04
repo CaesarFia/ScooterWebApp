3b7b212b0632d9dc603771fa0e07996b8688edb5
Caesar Fiallos <87624782+CaesarFia@users.noreply.github.com>
2024-08-04 00:17:09 +0000

    Conflicts resolved

--- src/components/MapComponent.svelte
+++ src/components/MapComponent.svelte
@@ -1,7 +1,6 @@
 <script lang='ts'>
     import { onMount } from 'svelte';
     import mapboxgl from 'mapbox-gl';
-    // import 'mapbox-gl/dist/mapbox-gl.css'; 
   
     let map: mapboxgl.Map;
   const mapboxAccessToken = 'pk.eyJ1IjoiY2Flc2FyZmlhIiwiYSI6ImNseTdxanEwNzA5aXQycW9lMDh6YmFrc2UifQ.CpwPntFIV276Nb755rg-Pg';
@@ -13,31 +12,9 @@
       container: 'map',
       style: 'mapbox://styles/caesarfia/clxzna0zz006q01p80zkl9v03'
     });
-
-    // example locations:
-
-    // interface Location {
-    //     latitude: number;
-    //     longitude: number;
-    // }
-
-    // const locations: Location[] = [
-    //     { latitude: 29.650201, longitude: -82.340019 }, 
-    //     { latitude: 29.648469, longitude: -82.349935 }, 
-    //     { latitude: 29.637931, longitude: -82.368317 },  
-    // ];
-
-    // locations.forEach(location => {
-    //     new mapboxgl.Marker()
-    //         .setLngLat([location.longitude, location.latitude])
-    //         .addTo(map);
-    // });
   });
   </script>
-  
-  <style>
-  </style>
-  
+    
   <div id="map" class="absolute h-screen z-0 w-screen m-0 p-0">
   </div>
   
