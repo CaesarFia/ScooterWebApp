cd01987836e4ed497d46fcd824e6e97cf27043fc
Jaidenmagnan <jaidenmagnan@gmail.com>
2024-08-04 22:23:06 -0400

    sign out

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { LayoutData } from './$types';
-	
+
 	export let data: LayoutData;
 	const { user } = data;
 </script>
 
-<main>
-	<h1>Hello {user.firstname}!</h1>
+<div class="bg-gray-900 p-4 text-white flex justify-end items-center space-x-4">
+	<p class="text-lg">Hello, {user.firstname}</p>
 	<form method="post" action="/logout" use:enhance>
-		<button>Sign out</button>
+		<button
+			type="submit"
+			class="bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
+		>
+			Sign Out
+		</button>
 	</form>
-    <slot />
-</main>
+</div>
+
+<slot />
