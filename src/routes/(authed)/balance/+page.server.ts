12ef41824d188e345597a3803d57a7f61adcff9a
Jaidenmagnan <84941950+Jaidenmagnan@users.noreply.github.com>
2024-08-05 00:16:12 -0400

    balance add (#20)
    * page
    
    * sidebar
    
    * sidebar
    
    * sign out
    
    * color changes
    
    * Fix floating point rounding errors
    
    ---------
    
    Co-authored-by: Nikhil Iyer <iyer.h.nikhil@gmail.com>

---
+++ src/routes/(authed)/balance/+page.server.ts
@@ -0,0 +1,54 @@
+import { redirect, error, type Actions } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+
+import db from '$lib/db';
+import { customers, transactions } from '$lib/db/schema';
+import { generateIdFromEntropySize } from 'lucia';
+import { add } from '$lib/utils';
+
+export const actions: Actions = {
+    add_balance: async (event) => {
+        if (!event.locals.user) {
+            error(403, { message: 'Forbidden' });
+        }
+
+        if (event.locals.user.balance === null) {
+            error(404, { message: 'Not Found' });
+        }
+
+        const formData = await event.request.formData();
+        const amount = formData.get('amount')?.toString();
+        if (!amount) {
+            error(400, { message: 'Bad Request' });
+        }
+
+        const transaction_id = generateIdFromEntropySize(10);
+
+        await db.insert(transactions).values({
+            id: transaction_id,
+            customerId: event.locals.user.id,
+            employeeId: null,
+            amount: amount,
+        });
+        
+        await db.update(customers).set({ balance: add(amount, event.locals.user.balance) }).where(eq(customers.id, event.locals.user.id));
+        redirect(302, "/balance");
+
+    },
+};
+
+export async function load({ locals }) {
+    if (!locals.user) {
+        error(403, { message: 'Forbidden' });
+    }
+
+    const currentBalance = locals.user.balance;
+    const user = locals.user
+
+    return {
+        data: {
+            currentBalance,
+            user
+        }
+    };
+}
---
+++ src/routes/(authed)/balance/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Sidebar from '$lib/components/Sidebar.svelte';
+
+	export let data;
+	let isEmployee = data.user?.isAdmin;
+</script>
+
+<div class="flex justify-center items-center min-h-screen bg-gray-900">
+	<Sidebar {isEmployee} />
+	<form
+		class="max-w-md mx-auto p-6 bg-gray-800 text-white rounded-lg shadow-md"
+		action="?/add_balance"
+		method="POST"
+	>
+		<div class="mb-4 text-center">
+			<p>Your balance is: <span class="font-bold">{data.data.currentBalance}</span></p>
+		</div>
+
+		<div class="flex items-center space-x-2">
+			<input
+				type="number"
+				name="amount"
+				step="0.01"
+				placeholder="Add balance"
+				class="w-full px-4 py-2 text-gray-800 bg-gray-100 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+			/>
+			<button
+				type="submit"
+				class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring"
+			>
+				✓
+			</button>
+		</div>
+	</form>
+</div>
