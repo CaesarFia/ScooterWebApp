fe34488b976c4c0a675330cfce3d587bb7df9649
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2024-08-06 23:55:00 -0400

    Allow deleting customers

--- src/routes/(authed)/dashboard/+page.svelte
+++ src/routes/(authed)/dashboard/+page.svelte
@@ -261,6 +261,12 @@
 									<td>{user.users.lastname}</td>
 									<td>{user.users.email}</td>
 									<td>Customer</td>
+									<td class="bg-charchoal">
+									<form class="bg-charchoal" action="?/delete_user" method="POST">
+										<input type="hidden" name="id" value = {user.users.id}/>
+										<button type="submit" class="px-4 py-2 bg-tc text-white font-semibold rounded-md shadow-sm focus:outline-none hover:opacity-80">Delete</button>
+									</form>
+									</td>
 								</tr>
 							{/each}
 							{#each data.employeeList as user}
